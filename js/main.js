9332159db28931d542181d4ddca77895899b0c17
ikechukwu-peter <pitiyke@gmail.com>
2021-09-01 20:34:47 +0100

    feat: added response for the contact field

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const hamburger = document.querySelector('.menu')
 const navLinks = document.querySelectorAll('.links')
-const container = document.querySelector('.link-container')
-
-
+const container = document.querySelector('.link-container');
 
 hamburger.addEventListener('click', (e)=> {
 hamburger.classList.toggle('is-active')
@@ -42,3 +40,25 @@ scrollBtn.addEventListener('click', () => {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
 
+
+// HANDLE FORM
+const form = document.getElementsByTagName('form')[0];
+   
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission, I will be in touch!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form, try again"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
