74c489dd28ed547caec95741a8be56e0915e7a04
ikechukwu-peter <pitiyke@gmail.com>
2021-09-03 04:41:06 +0100

    fix: removed overlay

--- js/main.js
+++ js/main.js
@@ -63,22 +63,4 @@ const form = document.getElementsByTagName('form')[0];
     }
     form.addEventListener("submit", handleSubmit)
    
-    //FOR LAX ANIMATION LIBRARY
-    window.onload = function () {
-      lax.init()
-  
-      // Add a driver that we use to control our animations
-      lax.addDriver('scrollY', function () {
-        return window.scrollY
-      })
-  
-      // Add animation bindings to elements
-      lax.addElements('.selector', {
-        scrollY: {
-          translateX: [
-            ["elInY", "elCenterY", "elOutY"],
-            [0, 'screenWidth/2', 'screenWidth'],
-          ]
-        }
-      })
-    }
+    
